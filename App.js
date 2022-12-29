0d131e0c1622d976e2e1f333ca0bf486dccbc927
r-caleb <calebakasa7@gmail.com>
2022-12-29 21:55:07 +0100

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+
+const tab = createBottomTabNavigator();
+
+function accueilScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Accueil</Text>
+    </View>
+  );
+}
+function parametreScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Parametre</Text>
+    </View>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            if (route.name == "Accueil") {
+              iconName = "home";
+            } else if (route.name == "Parametre") {
+              iconName = "settings";
+            }
+            return <Ionicons name={iconName} size={25}/>;
+          },
+        })}
+      >
+        <tab.Screen name="Accueil" component={accueilScreen} />
+        <tab.Screen name="Parametre" component={parametreScreen} />
+      </tab.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
